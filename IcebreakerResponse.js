25f393bc909b984b8a7e44455d67845fd3e4c8c4
Miriam Castillo <castillo.m.mc@gmail.com>
2017-12-16 03:14:14 +0000

    automatically backed up by learn

--- IcebreakerResponse.js
+++ IcebreakerResponse.js
@@ -4,7 +4,6 @@ function IcebreakerResponse(email){
 
 IcebreakerResponse.BatchCreate = function(emails){
   return emails.map(function(email){
-    return new IcebreakerResponse(emails[0])
+    return new IcebreakerResponse(emails)
   })
 }
-
